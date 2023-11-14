d8d093499cbc34eebf5963bb8e288041ef7dd214
Giovanni Vecchione <giovanni.vecchione@g.austincc.edu>
2023-11-13 20:34:11 -0600

    Updated 3.2

--- vecchione_Project3_Pt.2.py
+++ vecchione_Project3_Pt.2.py
@@ -5,7 +5,9 @@ Subject: Project 3 Pt. 2
 Description: Numerical Integration
 1. Find the area below the curve of f(x) = .34x^2 + 2.98x + 1.53 from x = 2 to x = 4 
 and above the x axis. Use the method of rectangles as described in class.
+
 2. Choose a tolerance, tol,  and an initial number of sub-intervals, n. 
+
 3. Calculate the approximate area. 
 
 Repeat the calculation with a greater n and continue the process until successive 
@@ -14,3 +16,35 @@ approximations have a difference less than tol.
 Report all choices and results.
 """
 
+def f(x):
+    return ((0.34 * x**2) + (2.98 * x) + 1.53)
+
+def approximate_area(a, b, n):
+    width = (b-a) / n
+    total_area = 0
+
+    for i in range(n):
+        height = f(a + i * width)
+        total_area += height * width
+    
+    return total_area
+
+#ask professor if this function is allowed, this ensures the error is below the tol
+def find_area_with_tolerance(a, b, initial_n, tol):
+    n = initial_n
+    previous_area = approximate_area(a, b, n)
+    while True:
+        n *= 2
+        current_area = approximate_area(a, b, n)
+        if abs(current_area - previous_area) < tol:
+            break
+        previous_area = current_area
+
+    return current_area, n
+
+a, b = 2, 4
+initial_n = 10  # initial number of sub-intervals
+tol = 0.001  # tolerance
+
+area, final_n = find_area_with_tolerance(a, b, initial_n, tol)
+print(f"Approximate area: {area}, Number of sub-intervals: {final_n}")
