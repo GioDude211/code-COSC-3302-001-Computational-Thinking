2e889445b290f402590f3720c5cff29060856235
Giovanni Vecchione <giovanni.vecchione@g.austincc.edu>
2023-11-13 21:00:51 -0600

    updated

--- vecchione_Project3_Pt.2.py
+++ vecchione_Project3_Pt.2.py
@@ -2,6 +2,7 @@
 Programmer: Giovanni Vecchione
 Date: 11/13/23
 Subject: Project 3 Pt. 2
+
 Description: Numerical Integration
 1. Find the area below the curve of f(x) = .34x^2 + 2.98x + 1.53 from x = 2 to x = 4 
 and above the x axis. Use the method of rectangles as described in class.
@@ -19,6 +20,7 @@ Report all choices and results.
 def f(x):
     return ((0.34 * x**2) + (2.98 * x) + 1.53)
 
+#determine if the total area is close to accurate
 def approximate_area(a, b, n):
     width = (b-a) / n
     total_area = 0
