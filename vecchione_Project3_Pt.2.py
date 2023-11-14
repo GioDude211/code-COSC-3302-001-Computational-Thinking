a5c647a0d851e33836cd6ba3d5d6f085d6e48db7
Giovanni Vecchione <giovanni.vecchione@g.austincc.edu>
2023-11-14 09:22:39 -0600

    further added comments

--- vecchione_Project3_Pt.2.py
+++ vecchione_Project3_Pt.2.py
@@ -16,37 +16,50 @@ approximations have a difference less than tol.
 
 Report all choices and results.
 """
-
+#function 1 that will pass the formula needed (f(x) = .34x^2 + 2.98x + 1.53)
+#returns the formula
 def f(x):
     return ((0.34 * x**2) + (2.98 * x) + 1.53)
 
-#determine if the total area is close to accurate
+#function 2 to determine the total area
+#returns the total_area
 def approximate_area(a, b, n):
-    width = (b-a) / n           #calculates width
-    total_area = 0              #init. variable to hold total area
+    width = (b-a) / n                   #calculates width
+    total_area = 0                      #init. variable to hold total area
 
-    for i in range(n):          #iterates over given sub-intervals
-        height = f(a + i * width)
-        total_area += height * width
+    for i in range(n):                  #iterates over given sub-intervals
+        height = f(a + i * width)       #calculates height
+        total_area += height * width    #calculates total area
     
-    return total_area           #returns calculated area
+    return total_area                   #returns total area
 
-#ask professor if this function is allowed, this ensures the error is below the tol
+#function 3 checks if total_area is within tolerance, otherwise it will iterate the while loop until its within acceptable range of error.
+#in basic terms its the refinement process, it will further divide into rectangles until the total_area is within tolerance.
+#returns the current_area and number of sub-intervals required
 def find_area_with_tolerance(a, b, initial_n, tol):
     n = initial_n
-    previous_area = approximate_area(a, b, n)
-    while True:
-        n *= 2
-        current_area = approximate_area(a, b, n)
-        if abs(current_area - previous_area) < tol:
+    previous_area = approximate_area(a, b, n)       #saves an iteration of calculating the area to a variable
+                                                    #this will determine if another iteration of the while loop is needed
+
+    #this loop determines if the absolute value of the difference between the current_area and previous_area is within appropriate tolerance
+    while True: 
+        n *= 2                                      #This is used to double the number of sub-intervals in each iteration of the while loop
+        current_area = approximate_area(a, b, n)   
+        if abs(current_area - previous_area) < tol: #checks area with tolerance
             break
         previous_area = current_area
 
     return current_area, n
 
-a, b = 2, 4
+a, b = 2, 4     #set start and end of range
 initial_n = 10  # initial number of sub-intervals
-tol = 0.001  # tolerance
+tol = 0.001     # tolerance
 
 area, final_n = find_area_with_tolerance(a, b, initial_n, tol)
 print(f"Approximate area: {area}, Number of sub-intervals: {final_n}")
+
+
+"""
+
+
+"""
